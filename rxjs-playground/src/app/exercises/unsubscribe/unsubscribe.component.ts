d88c55c5b97e2fccc110c2887ac3a8197dd64f1f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-05-22 11:02:18 +0200

    rxjs: toSignal

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil, map } from 'rxjs';
 import { HistoryComponent } from '../../shared/history/history.component';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
@@ -13,4 +13,7 @@ import { AsyncPipe } from '@angular/common';
 export class UnsubscribeComponent {
 
   interval$ = timer(0, 1000).pipe(map(x => x * 100));
+
+
+  interval$$ = toSignal(timer(0, 1000).pipe(map(x => x * 100)));
 }
