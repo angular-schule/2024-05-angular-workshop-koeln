ff441eec1aed1cf2488e5bf174e155431dabc758
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-05-22 14:45:19 +0200

    ngrx: actions

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { BookActions } from './books/store/book.actions';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Book Rating';
+
+
+  constructor(store: Store) {
+
+    store.dispatch(BookActions.loadBooks())
+
+  }
 }
